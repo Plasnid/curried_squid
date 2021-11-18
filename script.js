da731782f4d7b850ea3fbadb82b1c0638cca57f1
Plasnid <stuart@sackler.ca>
2021-11-18 16:13:12 -0500

    added more explanation

--- script.js
+++ script.js
@@ -40,42 +40,42 @@ function buildCephalopod(speciesName){
         }
     }
 }
-let spirulida = buildCephalopod("Spirulida")(true)(buildAppendages(2)(true)(),buildAppendages(8)(true)("ends"));
+let spirulida = buildCephalopod("Spirulida")(true)(buildAppendages(2)(true)(), buildAppendages(8)(true)("ends"));
 console.log(spirulida);
 let octopoda = buildCephalopod("Octopoda")(false)(buildAppendages(8)(true)(),"");
 console.log(octopoda);
 
-/*
-Q1. update the buildCephalopod function to take in a description
-(2 marks)
+/** 
+* * Q1. update the buildCephalopod function to take in a description in a curried layer
+* * (2 marks)
 */
 
-/*
-Q2.  update the buildCephalopod function to take in a mood in the form of a number from 0 to 1
-(2 marks)
+/** 
+* * Q2.  update the buildCephalopod function to take in a mood in a curried layer in the form of a number from 0 to 1
+* * (2 marks)
 */
 
-/*
-Q3.  Build 2 cephalopods from the image in the project using the functions listed above
-(10 marks)
+/** 
+* * Q3.  Build 2 cephalopods from the image in the project using the functions listed above
+* * (10 marks)
 */
 
-/*
-Q4. Invent 2 cephalopods and make instances of them from the functions above
-(10 marks)
+/** 
+* * Q4. Invent 2 cephalopods and make instances of them from the functions above
+* * (10 marks)
 */
 
-/*
-Q5.  Make a function called petCephalopod, which will use the mood value of an instantiated cephalopod and math.random().  
+/**
+* * Q5.  Make a function called petCephalopod, which will use the mood value of an instantiated cephalopod and math.random().  
 
-petCephalopod must:
-a.  return a phrase to be console logged
-b.  if mood is equal to or greater than the math.random() use template literals to show the name of the cephalopod, its mood, and that it liked being petted
-c.  if the mood is less than math.random(), return a phrase with the name of the cephalopod, showing the animal biting and being mad
-(5 marks)
+* * petCephalopod must:
+* * a.  return a phrase to be console logged
+* * b.  if mood is equal to or greater than the math.random() use template literals to show the name of the cephalopod, its mood, and that it liked being petted
+* * c.  if the mood is less than math.random(), return a phrase with the name of the cephalopod, showing the animal biting and being mad
+* *(5 marks)
 */
 
-/*
-Q6. run the petCephalopod function on each cephalopod using apply, call and bind. Be sure to console log each result.
-(20 marks)
+/**
+* *Q6. run the petCephalopod function on each cephalopod you invented using apply, call and bind. Be sure to console log each result.
+* *(20 marks)
 */
